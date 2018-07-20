ba6692bdc2b6b24c3a955ef221f4682301dfcebb
kmbruce1986 <kmbruce1986@gmail.com>
2018-07-20 11:24:46 +0100

    Questions 1-5

--- pet_shop.rb
+++ pet_shop.rb
@@ -1,3 +1,16 @@
 def pet_shop_name(array)
   return array[:name]
 end
+
+def total_cash(array)
+  return array[:admin][:total_cash]
+end
+
+def add_or_remove_cash(array, cash)
+  current_cash = 0
+  updated_cash = array[:admin][:total_cash] += cash
+end
+
+def pets_sold(array)
+  return array[:admin][:pets_sold]
+end 
