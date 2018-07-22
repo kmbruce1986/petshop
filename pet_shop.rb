c923e8b230515ad68ecaef6b035a9a59585eff00
kmbruce1986 <kmbruce1986@gmail.com>
2018-07-22 20:45:54 +0100

    first two optional questions

--- pet_shop.rb
+++ pet_shop.rb
@@ -71,3 +71,48 @@ end
 def add_pet_to_customer(customer, pet)
   customer[:pets].push(pet)
 end
+
+def customer_can_afford_pet(customer, pet)
+  if customer[:cash] >= pet[:price]
+    return true
+  else return false
+  end
+end
+
+# def sell_pet_to_customer(customer, petshop, pet)
+#   pet_to_be_sold = []
+#
+# # decide which pet is to be sold.
+#   for pet in petshop[:pets]
+#     if pet[:name] == pet
+#     pet_to_be_sold.push()
+#     end
+#   end
+#
+#   cost_of_pet = pet_to_be_sold[:price]
+#   petshop[:pets].delete(pet_to_be_sold)
+#   customer[:name].push(pet_to_be_sold)
+#   customer[:pets] += 1
+#   petshop[:admin][:pets_sold] -= 1
+#   customer[:cash] - cost_of_pet
+#   petshop[:admin][:total_cash] + cost_of_pet
+#
+# end
+
+
+# # sell one of the pets
+# petshop[:pets]
+# # to a customer
+# customer[:name]
+#
+# # increase the number of pets the customer has
+# customer[:pets]
+#
+# # increase the number of pets the petshop sold
+# petshop[:admin][:pets_sold]
+#
+# # decrease the cash of the customer
+# customer[:cash]
+#
+# # increase the cash of the petshop
+# petshop[:admin][:total_cash]
